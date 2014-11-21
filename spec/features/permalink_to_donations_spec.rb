ac2351202672387a02a2eea3e965e60228c1a27a
Jorge Gomez Sancha <jorge@bebanjo.com>
2014-11-21 13:36:34 +0100

    First spec covering existing donation tracking

--- spec/features/permalink_to_donations_spec.rb
+++ spec/features/permalink_to_donations_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature 'wadus' do
-  scenario 'creates a project' do
-    a = create_project
-    expect(a.name).to eq('Wikiwadus')
+RSpec.feature 'Tracking a donations from project page' do
+  scenario 'Shows a success message' do
+    create_project(:name => 'Wikiwadus')
+    user = create_user(:password => "wadusm4n")
+    visit '/login'
+    
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => "wadusm4n"
+    
+    click_button 'Log in'
+    
+    visit '/projects'
+    
+    click_link('Wikiwadus')
+        
+    fill_in 'donation_date', :with => "2014-10-10"
+    fill_in 'donation_quantity_cents', :with => "200"
+    click_button 'TrackDon'
+    
+    expect(page).to have_content 'Donation created! You are amazing'
   end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,13 +1,21 @@
 module Factories
   
   def create_project(attrs = {})
-    #<Project id: 1, name: "Wikipedia", description: "This is the description of the wikipedia. This is ...", url: "http://www.wikipedia.com", twitter: "wikipedia", donation_url: "https://donate.wikimedia.org/wiki/Special:Fundrais...", created_at: "2014-11-19 11:06:05", updated_at: "2014-11-19 11:06:05", slug: "wikipedia">
-    attrs[:name] ||= "Wikiwadus"
-    attrs[:description] ||= "A Wiki to track whatever you can think of"
-    attrs[:url] ||= "http://wikiwad.us"
-    attrs[:twitter] ||= "wikiwadus"
-    attrs[:donation_url] ||= "http://wikiwad.us/donate"
+    attrs[:name]          ||= "Wikiwadus"
+    attrs[:description]   ||= "A Wiki to track whatever you can think of"
+    attrs[:url]           ||= "http://wikiwad.us"
+    attrs[:twitter]       ||= "wikiwadus"
+    attrs[:donation_url]  ||= "http://wikiwad.us/donate"
     Project.create!(attrs)
   end
   
+  def create_user(attrs = {})
+    attrs[:name]      ||= "John Donor"
+    attrs[:username]  ||= "johndonor"
+    attrs[:email]     ||= "johndonor@example.com"
+    attrs[:country]   ||= "ES"
+    attrs[:password]              ||= "wadusm4n"
+    attrs[:password_confirmation] ||= attrs[:password]
+    User.create!(attrs)
+  end
 end
