50a2b13002b59448615ffff20525ae5c90a3d8f2
Fernando Blat <ferblape@gmail.com>
2014-12-05 20:00:20 +0100

    Cleanup deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,23 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
-# require 'mina_sidekiq/tasks'
-# require 'mina/unicorn'
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+require 'mina/rbenv'
 
 set :domain, '178.62.214.22'
 set :deploy_to, '/home/deploy/trackdons.org'
 set :repository, 'git@github.com:furilo/trackdons.git'
-set :branch, 'mina-deploy'
+set :branch, 'master'
+
 set :user, 'deploy'
 set :forward_agent, true
-# set :ssh_options, '-A'
 set :port, '5005'
-# set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-
-# For system-wide RVM install.
-# set :rvm_path, '/usr/local/rvm/bin/rvm'
-
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log', 'config/secrets.yml']
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
   invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
@@ -57,13 +31,10 @@ task :setup => :environment do
   queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
   queue %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
 end
- 
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -72,18 +43,8 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      invoke :'passenger:restart'
+      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
   end
 end
-
-desc "Restarts the nginx server."  
-task :restart do  
-  invoke :'passenger:restart'
-end
-
-namespace :passenger do  
-  task :restart do
-    queue "mkdir #{deploy_to}/current/tmp; touch #{deploy_to}/current/tmp/restart.txt"
-  end
-end  
