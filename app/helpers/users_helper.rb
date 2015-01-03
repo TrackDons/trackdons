028d9edadcad9eb047d940cb58f4b03bf18079bc
Fernando Blat <ferblape@gmail.com>
2015-01-03 16:21:53 +0100

    Restore avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,12 +4,11 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    gravatar_url = '#'
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def has_donations?
-    logged_in? && current_user.donations.count > 0
+    logged_in? && current_user.donations.size > 0
   end
 
 end
