882cf0824c7bd571441a365b786d371c50bd7271
Álvaro Ortiz <alvaro@furilo.com>
2015-02-05 17:38:48 +0100

    Small things for first public release

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Trackdons
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.available_locales = [:en, :es, :fr]
+    config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = :es
     config.active_record.raise_in_transactional_callbacks = true
 
